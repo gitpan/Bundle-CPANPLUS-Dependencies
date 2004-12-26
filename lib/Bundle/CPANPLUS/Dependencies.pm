5c33cfacef756084a64391a40b1243f8f8fce1dd
Jos Boumans <kane@cpan.org>
2004-12-26 13:15:59 +0000

    Import of KANE/Bundle-CPANPLUS-Dependencies-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-CPANPLUS-Dependencies
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KANE/Bundle-CPANPLUS-Dependencies-0.01.tar.gz
    gitpan-cpan-author:       KANE
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/CPANPLUS/Dependencies.pm
@@ -0,0 +1,89 @@
+package Bundle::CPANPLUS;
+
+$VERSION = "0.01";
+
+
+__END__
+
+=head1 NAME
+
+Bundle::CPANPLUS -- All modules required & desired by CPANPLUS
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Bundle::CPANPLUS::Dependencies'
+
+    perl -MCPAN -e 'install Bundle::CPANPLUS::Dependencies'
+
+=head1 DESCRIPTION
+
+Bundle to install all modules required & desired by CPANPLUS 
+
+=head1 CONTENTS
+
+Archive::Extract 0.07
+
+Archive::Tar 1.23
+
+Archive::Zip
+
+Bundle::CPANPLUS::Test::Reporter
+
+Compress::Zlib
+
+Digest::MD5
+
+File::Fetch 0.06
+
+IO::Zlib 1.04
+
+IPC::Cmd 0.24
+
+IPC::Run 0.77
+
+Locale::Maketext::Simple
+
+Log::Message
+
+LWP
+
+LWP::UserAgent
+
+Module::Build 0.2607
+
+Module::CoreList 1.97
+
+Module::Load 0.10
+
+Module::Load::Conditional 0.06
+
+Module::Pluggable 2.4
+
+Module::Signature 0.44
+
+Net::FTP
+
+Object::Accessor 0.03
+
+Params::Check 0.22
+
+Storable
+
+Term::ReadLine::Perl
+
+Term::UI 0.04
+
+Test::More
+
+URI
+
+YAML
+
+=head1 AUTHOR
+
+Jos Boumans <kane@cpan.org>
+
+=cut 
+
+
+__END__
